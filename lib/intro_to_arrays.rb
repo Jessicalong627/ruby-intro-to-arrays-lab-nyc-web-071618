dcc10e440ac251c45495f162d97df9d5a1808357
Jessica Long <jessicalong627@gmail.com>
2018-07-03 18:52:18 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -34,5 +34,6 @@ def last_element_with_array_methods(last_country)
 end
 
 def length_of_array(array)
-  
+  @programming_languages = ["Ruby", "Javascript", "Python", "C++", "Java", "Lisp", "PHP", "Clojure"]
+  @programming_languages.length
 end
