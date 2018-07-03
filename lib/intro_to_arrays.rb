0e7b878a5bdac99fe3916e7c8a15db0a782a8f24
Jessica Long <jessicalong627@gmail.com>
2018-07-03 18:41:54 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -2,3 +2,8 @@ def instantiate_new_array
   instantiate_new_array = []
   @my_new_array = instantiate_new_array
 end
+
+def instantiate_new_array
+  instantiate_new_array = []
+  @my_new_array = instantiate_new_array
+end
